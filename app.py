8055c5116934cdca292f881ccad51a005d863b69
Yusuf Karim <yusuf.karim@student.htw-berlin.de>
2023-08-03 22:06:48 +0200

    Daten sollten persistent gespeichert werden
    Frage: Beim neustarten des Servers verschwinden alle Einträge. Gibt es eine professionelle Möglichkeit Daten zu persistieren?
    # ChatGpt gab eine Liste von möglichkeiten an.
    Frage: Die 2.te Variante hört sich sehr gut an. Wie müsste ich meinen Code anpassen, um SQLAlchemy und MySQL zu verwenden?
    # Beim ausführen gab es einen Fehler.
    Frage: Ich bekomme ein 'Working outside of application context'. Wie kann ich diesen Fehler beheben?
    # Dann haben wir eine mysql datenbank mit benutzer einrichten wollen
    Frage: wie kann ich in mysql einen neuen benutzer erstellen und eine datenbank für diesen freigeben?

--- app.py
+++ app.py
@@ -1,53 +1,58 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://user_version_2:pw_version_2@localhost/ba_version_2'  # Datenbankpfad
+db = SQLAlchemy(app)
 
-students = []
-theses = []
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
 
+class Thesis(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    student = db.relationship('Student', backref=db.backref('theses', lazy=True))
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', students=students, theses=theses)
-
+    students = Student.query.all()
+    return render_template('index.html', students=students)
 
 @app.route('/add_student', methods=['POST'])
 def add_student():
     student_name = request.form['name']
-    students.append({'name': student_name, 'theses': []})
+    new_student = Student(name=student_name)
+    db.session.add(new_student)
+    db.session.commit()
     return redirect(url_for('index'))
 
-
-@app.route('/remove_student/<student_name>')
-def remove_student(student_name):
-    for student in students:
-        if student['name'] == student_name:
-            students.remove(student)
-            break
+@app.route('/remove_student/<int:student_id>')
+def remove_student(student_id):
+    student = Student.query.get_or_404(student_id)
+    db.session.delete(student)
+    db.session.commit()
     return redirect(url_for('index'))
-
 
 @app.route('/add_thesis', methods=['POST'])
 def add_thesis():
     thesis_title = request.form['title']
-    student_name = request.form['student']
-
-    for student in students:
-        if student['name'] == student_name:
-            student['theses'].append(thesis_title)
-            break
-
+    student_id = int(request.form['student'])
+    new_thesis = Thesis(title=thesis_title, student_id=student_id)
+    db.session.add(new_thesis)
+    db.session.commit()
     return redirect(url_for('index'))
 
-
-@app.route('/remove_thesis/<student_name>/<thesis_title>')
-def remove_thesis(student_name, thesis_title):
-    for student in students:
-        if student['name'] == student_name:
-            student['theses'].remove(thesis_title)
-            break
+@app.route('/remove_thesis/<int:thesis_id>')
+def remove_thesis(thesis_id):
+    thesis = Thesis.query.get_or_404(thesis_id)
+    db.session.delete(thesis)
+    db.session.commit()
     return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
     app.run(debug=True)
