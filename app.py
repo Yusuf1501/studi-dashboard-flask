db7c92e119d6962c358e8323131d0915e27be670
Yusuf Karim <yusuf.karim@student.htw-berlin.de>
2023-08-03 21:21:23 +0200

    Thesen sollten den Studenten zugeordnet werden
    Frage: Die Abschlussarbeiten müssen den jeweiligen Studenten zugeordnet werden können. Außerdem sollten die Action Button hinter den jeweiligen Einträgen stehen. Wie könnte man diese Punkte umsetzen?

--- app.py
+++ app.py
@@ -14,26 +14,38 @@ def index():
 @app.route('/add_student', methods=['POST'])
 def add_student():
     student_name = request.form['name']
-    students.append(student_name)
+    students.append({'name': student_name, 'theses': []})
     return redirect(url_for('index'))
 
 
 @app.route('/remove_student/<student_name>')
 def remove_student(student_name):
-    students.remove(student_name)
+    for student in students:
+        if student['name'] == student_name:
+            students.remove(student)
+            break
     return redirect(url_for('index'))
 
 
 @app.route('/add_thesis', methods=['POST'])
 def add_thesis():
     thesis_title = request.form['title']
-    theses.append(thesis_title)
+    student_name = request.form['student']
+
+    for student in students:
+        if student['name'] == student_name:
+            student['theses'].append(thesis_title)
+            break
+
     return redirect(url_for('index'))
 
 
-@app.route('/remove_thesis/<thesis_title>')
-def remove_thesis(thesis_title):
-    theses.remove(thesis_title)
+@app.route('/remove_thesis/<student_name>/<thesis_title>')
+def remove_thesis(student_name, thesis_title):
+    for student in students:
+        if student['name'] == student_name:
+            student['theses'].remove(thesis_title)
+            break
     return redirect(url_for('index'))
 
 
